79a5ecb349e0a1c4fd51165ab873bdd956ef7bd1
unknown <ravi.gohel2001@gmail.com>
2022-08-23 22:01:56 +0530

    Initial commit

---
+++ src/main/kotlin/question1.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var a : Int = 43
+    var b : Float = 25.5f
+    var c : Char = 'o'
+    var d : String = "Ravi"
+    var e : Boolean = true
+    var f : Double = 322.56669
+    var g : Long = 741852963L
+    var h : Short = -3
+    var i : Byte = 64
+
+    println("Integer Value : $a")
+    println("Float Value : $b")
+    println("Character Value : $c")
+    println("String Value : $d")
+    println("Boolean Value : $e")
+    println("Double Value : $f")
+    println("Long Value : $g")
+    println("Short Value : $h")
+    println("Byte Value : $i")
+}
